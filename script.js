2ab88e5026b0d9a0cb2b23c1264b10d019f3166d
Noseflute <30875233+Noseflute@users.noreply.github.com>
2017-08-14 09:16:15 -1000

    Update to Script.js
    Added extra questions

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function sayHello() {
   //I created a function called sayHello//
-  var userName = prompt("What is your name?");
+  var firstName = prompt("What is your first name?");
+  var middleName = prompt ("What is your middle name?");
+  var lastName= prompt ("What is your last name?")
   //I create a variable called userName and ask the user something//
-  window.alert("Hello " + userName);
+  window.alert("Hello " + firstName + " " + middleName + " " + lastName + " " + "I'm PICKLE RICKKKKKKKKKK");
   //I created a popup window teelling the userName hello.//
+
 }
