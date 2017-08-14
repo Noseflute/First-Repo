ae98b7ebded78c6ce0cbfe8068f86609ed42d187
Noseflute <30875233+Noseflute@users.noreply.github.com>
2017-08-14 08:56:05 -1000

    Added Button
    Button, script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  //I created a function called sayHello//
+  var userName = prompt("What is your name?");
+  //I create a variable called userName and ask the user something//
+  window.alert("Hello " + userName);
+  //I created a popup window teelling the userName hello.//
+}
