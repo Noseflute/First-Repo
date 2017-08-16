b6040ab3146dee9d0d36248cf8a93b4a55d3008b
Noseflute <30875233+Noseflute@users.noreply.github.com>
2017-08-16 09:24:47 -1000

    Multiplying
    8/16/17

---
+++ Multiplying.js
@@ -0,0 +1,9 @@
+//Ask user for a number
+//Ask user for another number
+//Take both numbers and multiply them
+//Output the value to the screen using a window popup
+
+var firstNumber = prompt ("Pick a number");
+var secondNumber = prompt ("Pick a number");
+//You can also add var thirdNumber = prompt (firstNumber*secondNumber)
+window.alert("Now multiplied" + " " + firstNumber * secondNumber);
